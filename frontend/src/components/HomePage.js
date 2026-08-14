e3a6363ba38f6564e65f04af296b19253ef8b8fb
izzroni <ronitmenon18@yahoo.com>
2026-08-14 11:58:09 +0530

    Add frontend/src/components/HomePage.js

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "./Navbar";
+import HeroCarousel from "./HeroCarousel";
+import HeroSection from "./HeroSection";
+import Footer from "./Footer";
+import "../styles/homePage.css";
+
+
+const HomePage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      // If no token is found, redirect to login page
+      navigate("/");
+    }
+  }, [navigate]);
+
+  return (
+    <div>
+      <Navbar />
+      <HeroCarousel />
+      <HeroSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
